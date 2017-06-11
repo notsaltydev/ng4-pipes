ff893c41c990ce79ea0024d0c1e7e1c81c402401
adamnieslony <notsaltydev@gmail.com>
2017-06-11 22:14:54 +0200

    230. Understanding the "async" Pipe

--- src/app/shorter.pipe.ts
+++ src/app/shorter.pipe.ts
@@ -1,7 +1,7 @@
 /**
  * Created by adamnieslony on 11.06.2017.
  */
-import {Pipe, PipeTransform} from "@angular/core";
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'shorten'
